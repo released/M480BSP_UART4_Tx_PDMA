7dc80a4a6b2d16e5a8ac481940eb94c9d2c242c9
Released <released.chen@gmail.com>
2023-03-13 13:30:10 +0800

    update
    update

--- SampleCode/Template/Keil/obj/misc_config.d
+++
@@ -1,4 +0,0 @@
-.\obj\misc_config.o: ..\misc_config.c
-.\obj\misc_config.o: ..\misc_config.h
-.\obj\misc_config.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\obj\misc_config.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
